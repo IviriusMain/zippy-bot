da29ca6ff5da58e5d11ca53a2294d42c14d4bd92
Zane <90309290+Zingzy@users.noreply.github.com>
2023-12-27 00:08:51 +0530

    Add poll command to poll_cog.py

--- cogs/poll_cog.py
+++ cogs/poll_cog.py
@@ -9,7 +9,12 @@ from api import app
 class polling(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
+    @app_commands.command(name="poll", description="Create a poll")
+    @app_commands.guild_only()
+    @app_commands.checks.cooldown(1, 300)
+    async def poll(self, interaction, question: str):
+        pass
 
 async def setup(bot):
     bot.add_cog(polling(bot))

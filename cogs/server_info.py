7a4a80afb1200709c5e9fe386157915121f0d243
Zane <90309290+Zingzy@users.noreply.github.com>
2024-09-30 03:21:21 +0530

    Add links command in server_info cog

--- cogs/server_info.py
+++ cogs/server_info.py
@@ -29,6 +29,25 @@ class server_info(commands.Cog):
 
         await interaction.response.send_message(embed=embed)
 
+    @app_commands.command(name="links", description="Get Links to various Ivirius Products")
+    @app_commands.guild_only()
+    async def links(self, interaction:discord.Interaction):
+        embed = discord.Embed(
+            title="Ivirius Links",
+            description="Here are some links to Ivirius products",
+            color=discord.Color.green()
+        )
+
+        embed.add_field(name="Website <:Ivirius:1290061289258745896>", value="https://ivirius.vercel.app/", inline=False)
+        embed.add_field(name="Rebound 11 <:Rebound11:1290061222494081075>", value="https://ivirius.vercel.app/rebound11", inline=False)
+        embed.add_field(name="Ivirius Text Editor Plus <:TextEditorPlus:1290061177140936704>", value="https://ivirius.vercel.app/ivirius-text-editor-plus", inline=False)
+        embed.add_field(name="Ivirius Text Editor <:TextEditor:1290061049352949791>", value="https://ivirius.vercel.app/ivirius-text-editor/", inline=False)
+        embed.add_field(name="Crimson UI <:CrimsonUI:1290061257361064062>", value="https://ivirius.vercel.app/crimsonui", inline=False)
+
+        embed.set_footer(text=f"Requested by {interaction.user.display_name}", icon_url=interaction.user.avatar.url if interaction.user.avatar else interaction.user.default_avatar.url)
+
+        await interaction.response.send_message(embed=embed)
+
 async def setup(bot):
     await bot.add_cog(server_info(bot))
     print("Loaded server_info.py")
